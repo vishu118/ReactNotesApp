a489cf69075cb61924f332ecbf91943160ecec4f
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-01 08:44:41 +0530

    First commit

---
+++ src/Components/Note.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+const Note = () => {
+  return (
+    <div className='note'>
+        <span>Hello! This is our Note</span>
+        <div className='note-footer'>
+                <samall>13/04/2021</samall>
+               <i class="fa-solid fa-trash-can"></i>
+        </div>
+
+    </div>
+  )
+}
+
+export default Note
---
+++ src/Components/NotesList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Note from './Note'
+
+
+const NotesList = () => {
+  return (
+    
+    <div className="notes-List">
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+    </div>
+  )
+}
+
+export default NotesList
